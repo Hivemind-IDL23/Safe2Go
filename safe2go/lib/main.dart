90adfc92faec04c8baef59f843df527b011ea8df
yashbim <ybimsara03@gmail.com>
2023-09-09 18:29:15 +0530

    Google option added

--- safe2go/lib/main.dart
+++ safe2go/lib/main.dart
@@ -103,6 +103,43 @@ class LogoScreen extends StatelessWidget {
                 ),
               ),
             ),
+
+            SizedBox(height: 20.0), // Add spacing below the "Sign up" Button
+
+            // Sign In with Google Button
+            Container(
+              width: 200.0,
+              height: 40.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                color: Colors.red, // Customize the background color
+              ),
+              child: ElevatedButton.icon(
+                onPressed: () {
+                  // Implement Google Sign-In logic here
+                },
+                icon: Image.asset(
+                  'assets/google_icon.png', // Replace 'google_icon.png' with the path to your Google icon image asset
+                  width: 24.0, // Adjust the width as needed
+                  height: 24.0, // Adjust the height as needed
+                  color: Colors.white, // Customize the icon color
+                ),
+                label: Text(
+                  'Sign In with Google',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16.0,
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary:
+                      Color(0xFFFF159A9C), // Match the button background color
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
--- safe2go/lib/temp.dart
+++ safe2go/lib/temp.dart
@@ -1,1 +1,32 @@
 
+// Sign In with Google Button
+Container(
+  width: 200.0,
+  height: 40.0,
+  decoration: BoxDecoration(
+    borderRadius: BorderRadius.circular(10.0),
+    color: Colors.red, // Customize the background color
+  ),
+  child: ElevatedButton.icon(
+    onPressed: () {
+      // Implement Google Sign-In logic here
+    },
+    icon: Icon(
+      Icons.g_translate, // Replace with Google icon or desired icon
+      color: Colors.white, // Customize icon color
+    ),
+    label: Text(
+      'Sign In with Google',
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 16.0,
+      ),
+    ),
+    style: ElevatedButton.styleFrom(
+      primary: Colors.red, // Match the button background color
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    ),
+  ),
+),
